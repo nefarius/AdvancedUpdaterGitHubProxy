d9e3f031ee32abb267b33da95e3e545e0296f389
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-06-23 21:18:40 +0200

    Removed tests

--- tests/AdvancedUpdaterGitHubProxy.E2E.Tests/Usings.cs
+++
@@ -1,8 +0,0 @@
-// Libraries
-global using Xunit;
-global using Microsoft.AspNetCore.Mvc.Testing;
-global using FluentAssertions;
-// HTTP Status Codes and JSON
-
-// Custom
-global using AdvancedUpdaterGitHubProxy.E2E.Tests.Extensions;
